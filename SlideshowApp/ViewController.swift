786c690cfc4a99669323b00c3b4ed7f9104c3c4b
mayudong <mayudong200333@gmail.com>
2019-10-13 16:50:11 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 馬煜東 on 2019/10/13.
+//  Copyright © 2019年 ikutou.ba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
